38d4d41da96efe3e1d3af9ef94ff8e590add245f
zalazarc20 <zalazarc20@gmail.com>
2022-11-25 16:27:17 -0300

    change on style and add json

--- js/app.js
+++ js/app.js
@@ -5,29 +5,6 @@ const btnNext = document.getElementById('next-channel');
 const btnPrev = document.getElementById('prev-channel');
 const channelName = document.getElementById('channel');
 const messageContainer = document.getElementById('message-channel');
-
-const channels = [
-    {
-        name: 'dSports',
-        url: 'https://dtvott-abc.akamaized.net/dash_live_1061/manifest.mpd?&ck=eyI3MTE5MDY5Yjk5MDE1YjRlYjllZTQzMDA1NzczOTllMiIgOiAiYjg5ODI3Y2M5NzBjZDc4ZGE4ODJmZmNhMzUwNGM3NzQifQ=='
-    },
-    {
-        name: 'tycSports',
-        url: 'https://1240-vos.dtvott.com/DASH/manifest.mpd?&ck=eyI0ZDQ1Yjc5ZDQ0ODczNDM1ODkwNThhYjQ5ZGRmOGNmMCI6ImFhN2Y5NWY3MWEzY2ZlNDBhYTU5OTA0ZjkyODVhZjcyIn0='
-    },
-    {
-        name: 'tvPublica',
-        url: 'https://edge-live17-sl.cvattv.com.ar/live/c6eds/Canal7/SA_Live_dash_enc_2A/Canal7.mpd?&ck=eyJjYzhjODJhYzJlYzdlOTc5OTUyN2MyOWRiNzM1NGU4MSIgOiAiY2M0YWFlMTczZGQyZWYxN2FlMjZiZTNmN2FlODc2NjIifQ=='
-    },
-    {
-        name: 'deporTv',
-        url: 'https://dtvott-abc.akamaized.net/dash_live_1056/manifest.mpd?&ck=eyIwMGE3NzVkMWI5NmI1OTUxYjNjM2FiNTc5OWY5ODY4ZSIgOiAiM2E1NmRmMDA0N2RkNzA5Mzg4YzYwNmY5ZmYyZmJhZGQifQ=='
-    },
-    {
-        name: 'azteca7',
-        url: 'https://live4-ott.izzigo.tv/out/u/dash/AZTECA-7-HD/default.mpd?&ck=eyJjZThmODZkMmYwZDE2Mzg1NzQyMzdhZGE4YWMyZTgyYSIgOiAiNjQyNzcyZWJmMTgzMmYxZjJmYTU4Y2I3ZThmY2ZkMDUifQ=='
-    },
-]
 let count = 0;
 
 // functions =============================================
@@ -39,13 +16,16 @@ const detectIndex = (hash) => {
 const setLoading = () => {
     messageContainer.innerHTML = 'Cargando...';
     messageContainer.style.display = 'block';
-    messageContainer.style.backgroundColor = 'rgba(0,255,0,0.3)';
+    messageContainer.style.color = '#02730D';
+    messageContainer.style.backgroundColor = 'rgba(157,255,80,0.59)';
 }
+
+const addName = (name) => channelName.innerHTML = `${name}`;
 
 const changeChannel = (arr, i = 0) => {
     const channel = arr[i].name;
     location.hash = channel;
-    channelName.innerHTML = channel.toUpperCase();
+    addName(channel.toUpperCase());
     setLoading();
 }
 
@@ -65,7 +45,7 @@ const validHash = (hash) => {
 document.addEventListener('shaka-ui-loaded', async e => {
     const channel = channels[count].name
     location.hash = channel
-    channelName.innerHTML = channel.toUpperCase();
+    addName(channel.toUpperCase())
     setLoading();
     let {hash} = location;
 
---
+++ js/getChannels.js
@@ -0,0 +1,4 @@
+const getChannels = async () => {
+    const req = await fetch('');
+    return await req.json();
+}
