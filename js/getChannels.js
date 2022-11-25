e5e8e5dda83d58c821875b132cac92c5f0bf6fdc
zalazarc20 <zalazarc20@gmail.com>
2022-11-25 16:43:45 -0300

    finish

--- js/getChannels.js
+++
@@ -1,4 +0,0 @@
-export const getChannels = async () => {
-    const req = await fetch('https://raw.githubusercontent.com/zalazarc20/Mundial2022/main/channels/ch.json');
-    return await req.json();
-}
